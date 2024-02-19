7563e6f92094fc347a74739568b5e7e875287c1f
MA Jian <majian1979@qq.com>
2024-02-20 05:14:01 +0800

    Update nile.py

--- nile.py
+++ nile.py
@@ -2,7 +2,6 @@ import copent
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 nile = [1120,1160,963,1210,1160,1160,813,1230,1370,1140,995,935,1110,
 994,1020,960,1180,799,958,1140,1100,1210,1150,1250,1260,1220,1030,1100,
 774,840,874,694,940,833,701,916,692,1020,1050,969,831,726,456,824,702,
@@ -17,8 +16,8 @@ print(pos)
 
 plt.subplot(2,1,1)
 plt.plot(year,nile)
-m1 = np.mean(nile[0:pos])
-m2 = np.mean(nile[(pos+1):])
+m1 = np.mean(nile[0:pos-1])
+m2 = np.mean(nile[pos:])
 pos = pos + 1870
 plt.plot([1871,pos],[m1,m1], linestyle = '--', color = 'r')
 plt.plot([pos+1,len(nile)+1871],[m2,m2], linestyle = '--', color = 'r')
@@ -26,4 +25,3 @@ plt.subplot(2,1,2)
 plt.plot(year,stat)
 plt.plot([pos,pos],[min(stat),max(stat)],linestyle = '--', color = 'r')
 plt.show()
-
